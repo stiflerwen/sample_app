be48ba1ea27a372c3fcfa64cc9232793a1bd74ac
stiflerwen <info@la2class.tk>
2017-04-05 17:00:25 +0500

    Add sign up page

---
+++ spec/factories.rb
@@ -0,0 +1,8 @@
+FactoryGirl.define do
+  factory :user do
+    name     "Dmitriy Tcepilov"
+    email    "dvtcepilov@gmail.com"
+    password "Stifler59"
+    password_confirmation "Stifler59"
+  end
+end
