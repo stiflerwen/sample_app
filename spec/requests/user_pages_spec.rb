8440a68d725f346c2fc95a3eb65d50888e7febae
stiflerwen <info@la2class.tk>
2017-04-13 16:27:39 +0500

    Edit Updating Users

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -8,9 +8,6 @@ describe "User pages" do
     let(:user) { FactoryGirl.create(:user) }
     before { visit user_path(user) }
 
-    #it { should have_content('Sign up') }
-    #it { should have_title(full_title('Sign up')) }
-
     it { should have_content(user.name) }
     it { should have_title(user.name) }
   end
@@ -18,13 +15,16 @@ describe "User pages" do
   describe "signup page" do
     before { visit signup_path }
 
+    it { should have_content('Sign up') }
+    it { should have_title(full_title('Sign up')) }
+
     let(:sumbit) { "Create my account" }
 
     describe "invalid information" do
     	it "should not create a user" do
     		expect { click_button sumbit }.not_to change(User, :count)
-    	end
     end
+  end
 
   describe "valid information" do
   	before do
@@ -44,8 +44,46 @@ describe "User pages" do
 
         it { should have_link('Sign out') }
         it { should have_title(user.name) }
-        it { should have_selector('div.alert.alert-success', text: 'Welcome') }
-      end
+        it { should have_selector('div.alert.alert-', text: 'Welcome') }
+    end
   end
 end
+
+  describe "edit" do
+    let(:user) { FactoryGirl.create(:user) }
+    before do
+      sign_in user
+      visit edit_user_path(user)
+    end
+
+    describe "page" do
+      it { should have_content("Update your profile") }
+      it { should have_title("Edit user") }
+      it { should have_link('change', href: 'http://gravatar.com/emails') }
+    end
+
+    describe "with invalid information" do
+      before { click_button "Save changes" }
+
+      it { should have_content('error') }
+    end
+
+    describe "with valid information" do
+      let(:new_name)  { "New Name" }
+      let(:new_email) { "new@example.com" }
+      before do
+        fill_in "Name",             with: new_name
+        fill_in "Email",            with: new_email
+        fill_in "Password",         with: user.password
+        fill_in "Confirm Password", with: user.password
+        click_button "Save changes"
+      end
+
+      it { should have_title(new_name) }
+      it { should have_selector('div.alert.alert-') }
+      it { should have_link('Sign out', href: signout_path) }
+      specify { expect(user.reload.name).to  eq new_name }
+      specify { expect(user.reload.email).to eq new_email }
+    end
+  end
 end
