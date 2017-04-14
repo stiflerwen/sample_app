e65a6ad07aed1b14260e7d90f7ac9eb03c164ff0
stiflerwen <info@la2class.tk>
2017-04-14 16:57:59 +0500

    Finish user edit, update, index, and destroy actions

--- spec/factories.rb
+++ spec/factories.rb
@@ -1,8 +1,12 @@
 FactoryGirl.define do
   factory :user do
-    name     "Dmitriy Tcepilov"
-    email    "dvtcepilov@gmail.com"
-    password "Stifler59"
-    password_confirmation "Stifler59"
+    sequence(:name)  { |n| "Person #{n}" }
+    sequence(:email) { |n| "person_#{n}@example.com"}
+    password "foobar"
+    password_confirmation "foobar"
+
+    factory :admin do
+    	admin true
+    end
   end
 end
